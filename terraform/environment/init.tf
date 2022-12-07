178cf5e6949bb7da0168a401c2aa3794850c0970
Tim Sawyer <tim.git@hotmandarins.co.uk>
2022-12-07 21:13:47 +0000

    Remove secret from terraform

--- terraform/environment/init.tf
+++ terraform/environment/init.tf
@@ -12,7 +12,6 @@ terraform {
     resource_group_name      = "bbr5-global"
     storage_account_name     = "bbr5state"
     container_name           = "terraform"
-    key                      = "terraform.tfstate"
-    access_key               = "6APS1CtguUV+bReXXF9vGjww6QxQMf6CDWKKmvEd+d+RbbH+a53QU0ROxDKU57iDR0weQdei8Svt+ASt0sz4Yw=="
+    key                      = "bbr-terraform.tfstate"
   }
 }
