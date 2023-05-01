a48e94b2c772c00065d3533e61960c13a1d8d382
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-05-01 12:08:15 +0100

    404 page

--- site/src/main/java/uk/co/bbr/services/framework/NotFoundException.java
+++ site/src/main/java/uk/co/bbr/services/framework/NotFoundException.java
@@ -1,5 +1,9 @@
 package uk.co.bbr.services.framework;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Record not found")
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
