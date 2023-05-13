bc68e556a33af026eec6db7a46cfce2eb2d2bf3d
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-05-13 17:01:55 +0100

    Region winning bands

--- site/src/main/java/uk/co/bbr/web/regions/RegionController.java
+++ site/src/main/java/uk/co/bbr/web/regions/RegionController.java
@@ -10,13 +10,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import uk.co.bbr.services.bands.BandService;
 import uk.co.bbr.services.bands.dao.BandDao;
+import uk.co.bbr.services.bands.sql.dto.BandWinnersSqlDto;
 import uk.co.bbr.services.framework.NotFoundException;
 import uk.co.bbr.services.regions.RegionService;
 import uk.co.bbr.services.regions.dao.RegionDao;
 import uk.co.bbr.services.regions.dto.LinkSectionDto;
 import uk.co.bbr.services.regions.dto.RegionPageDto;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -26,6 +29,7 @@ import java.util.stream.Collectors;
 public class RegionController {
 
     private final RegionService regionService;
+    private final BandService bandService;
     private final ObjectMapper objectMapper;
 
     @GetMapping("/regions")
@@ -46,6 +50,14 @@ public class RegionController {
         return "regions/region";
     }
 
+    @GetMapping("/regions/{regionSlug}/contests")
+    public String regionContests(Model model, @PathVariable("regionSlug") String regionSlug) {
+        RegionPageDto region = this.regionService.fetchBySlugForPage(regionSlug);
+
+        model.addAttribute("Region", region);
+        return "regions/region-contests";
+    }
+
     @GetMapping("/regions/{regionSlug}/links")
     public String regionLinks(Model model, @PathVariable("regionSlug") String regionSlug) {
         Optional<RegionDao> region = this.regionService.fetchBySlug(regionSlug);
@@ -58,6 +70,23 @@ public class RegionController {
         model.addAttribute("Region", region.get());
         model.addAttribute("Sections", bandsBySection);
         return "regions/regionLinks";
+    }
+
+    @GetMapping("/regions/{regionSlug}/winners")
+    public String regionWinners(Model model, @PathVariable("regionSlug") String regionSlug) {
+        RegionPageDto region = this.regionService.fetchBySlugForPage(regionSlug);
+
+        List<BandWinnersSqlDto> bandWinners = this.bandService.fetchContestWinningBands();
+        List<BandWinnersSqlDto> winningBandsForRegion = new ArrayList<>();
+        for (BandWinnersSqlDto eachWinner : bandWinners) {
+            if (eachWinner.getRegionSlug().equals(region.getSlug())) {
+                winningBandsForRegion.add(eachWinner);
+            }
+        }
+
+        model.addAttribute("Region", region);
+        model.addAttribute("WinningBands", winningBandsForRegion);
+        return "regions/region-winners";
     }
 
     @GetMapping(value="/regions/{regionSlug}/{sectionType}/bands.json", produces="application/json")
