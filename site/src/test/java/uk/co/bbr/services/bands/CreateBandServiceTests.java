3ce2e61d102b5b5d40726286b69c3c1c1a325ca9
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-05-14 22:07:04 +0100

    Remove GIS dependency

--- site/src/test/java/uk/co/bbr/services/bands/CreateBandServiceTests.java
+++ site/src/test/java/uk/co/bbr/services/bands/CreateBandServiceTests.java
@@ -1,7 +1,6 @@
 package uk.co.bbr.services.bands;
 
 import org.junit.jupiter.api.Test;
-import org.locationtech.jts.geom.GeometryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
