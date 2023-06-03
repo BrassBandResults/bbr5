4a688687f636d4bc632f3e5d23c5a92a313a39d9
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-06-03 13:00:38 +0100

    Extract lookup javascript

---
+++ static/js2023/lookup.js
@@ -0,0 +1,34 @@
+function lookup(inputId, entity) {
+    let inputElement = document.getElementById(inputId);
+    let inputValue = inputElement.value;
+    if (inputValue.length > 2 ) {
+        inputElement.classList.remove("bg-success");
+        inputElement.classList.remove("text-white");
+        inputElement.classList.add("bg-warning");
+  
+        let searchList = document.getElementById('list-' + inputId);
+        if (searchList === null) {
+            searchList = document.createElement("ul");
+            searchList.id = 'list-' + inputId;
+            searchList.className="list-lookup";
+            inputElement.parentElement.append(searchList);
+        }
+  
+        const httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = () => {
+            if (httpRequest.readyState === XMLHttpRequest.DONE) {
+                if (httpRequest.status === 200) {
+                    let resultsHtml = "";
+                    let data = JSON.parse(httpRequest.responseText);
+                    for (let i = 0; i < data.matches.length; i++) {
+                        let displayText = "<b>" + data.matches[i].name + "</b> <small>" + data.matches[i].context + "</small>";
+                        resultsHtml += "<li style='cursor:pointer' onclick=\"fill('" + inputId + "', '" + data.matches[i].slug + "', '" + data.matches[i].name + "');\">" + displayText + "</li>";
+                    }
+                    searchList.innerHTML = resultsHtml;
+                }
+            }
+        };
+        httpRequest.open("GET", "/lookup/" + entity + "/data.json?s=" + inputValue, true);
+        httpRequest.send();
+    }
+}
