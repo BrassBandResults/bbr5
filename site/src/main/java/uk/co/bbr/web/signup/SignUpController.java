4b5af246dcd68dbc4fdc6d2074f9840ac5794283
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-06-21 21:26:35 +0100

    Photo for anti-spam check

---
+++ site/src/main/java/uk/co/bbr/web/signup/SignUpController.java
@@ -0,0 +1,52 @@
+package uk.co.bbr.web.home;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import uk.co.bbr.services.security.UserService;
+import uk.co.bbr.services.security.dao.BbrUserDao;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final UserService userService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        return "home/home";
+    }
+
+    @GetMapping("/statistics")
+    public String statistics() {
+        return "home/statistics";
+    }
+
+    @GetMapping("/faq")
+    public String faq() {
+        return "home/faq";
+    }
+
+    @GetMapping("/about-us")
+    public String aboutUs() {
+        return "home/about-us";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy() {
+        return "home/privacy";
+    }
+
+    @GetMapping("/leaderboard")
+    public String leaderboard(Model model) {
+
+        List<BbrUserDao> topUsers = this.userService.fetchTopUsers();
+
+        model.addAttribute("TopUsers", topUsers);
+
+        return "home/leaderboard";
+    }
+}
