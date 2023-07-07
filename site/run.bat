0ae07f38ece73b2526af8db4dfbb85d6bcb8d1d9
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-07-07 09:46:11 +0100

    Create group droplist working

---
+++ site/run.bat
@@ -0,0 +1,9 @@
+call mvnw clean install -Dmaven.test.skip=true
+set JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
+set BBR_SMTP_SERVER_PASSWORD=password
+set BBR_SMTP_SERVER_USERNAME=username
+set BBR_SMTP_SERVER_HOST=smtp.sendgrid.net
+set BBR_DATABASE_URL=jdbc:sqlserver://timpi;database=bbr;trustServerCertificate=true
+set BBR_DATABASE_USERNAME=sa
+set BBR_DATABASE_PASSWORD=admin9552Password
+call mvnw spring-boot:run
