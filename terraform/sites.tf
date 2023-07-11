c8d6384f7dc595f7ac113b065ae535b1dfe98b25
Alec <30310787+alec-pinson@users.noreply.github.com>
2023-07-11 10:32:24 +0100

    Add terraform apply and remove static apps + rework to allow forked secrets (#3)
    * Add terraform apply and remove static apps
    
    * rework to allow forked secrets

--- terraform/sites.tf
+++ terraform/sites.tf
@@ -1,11 +1,3 @@
-resource "azurerm_static_site" "docs" {
-  name                = "static-docs"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  sku_tier            = "Free"
-  sku_size            = "Free"
-}
-
 resource "azurerm_static_site" "apps" {
   name                = "static-apps"
   resource_group_name = azurerm_resource_group.this.name
