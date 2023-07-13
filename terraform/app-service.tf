ae1be41e2e345e00798b03f5e0d6ed4be7ca5b2d
Alec Pinson <30310787+alec-pinson@users.noreply.github.com>
2023-07-13 16:19:16 +0100

    [skip-ci] Fix PR environments

--- terraform/app-service.tf
+++ terraform/app-service.tf
@@ -6,7 +6,7 @@ resource "azurerm_service_plan" "bbr5plan" {
   sku_name            = "B1"
 }
 resource "azurerm_linux_web_app" "bbr5" {
-  name                = "bbr5"
+  name                = terraform.workspace == "prod" ? "bbr5" : "bbr5-${terraform.workspace}"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_service_plan.bbr5plan.location
   service_plan_id     = azurerm_service_plan.bbr5plan.id
