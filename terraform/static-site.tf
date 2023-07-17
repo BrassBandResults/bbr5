63cef1f6da1160738a34a704c024e1cc0168b485
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-07-17 09:27:27 +0100

    static web app unique name

--- terraform/static-site.tf
+++ terraform/static-site.tf
@@ -1,5 +1,5 @@
 resource "azurerm_static_site" "apps" {
-  name                = "static-apps"
+  name                = terraform.workspace == "prod" ? "static-apps" : "static-apps-${terraform.workspace}"
   resource_group_name = azurerm_resource_group.this.name
   location            = "westeurope"
   sku_tier            = "Free"
