34624bb2a8fc9e30c33dce09e108a2f855543f78
sawyert <tim.git@hotmandarins.co.uk>
2023-07-17 14:22:22 +0100

    Get Venue api (#48)
    * Get Venue api
    
    * Change path to newman tests

---
+++ site/src/main/java/uk/co/bbr/api/VenueApiController.java
@@ -0,0 +1,26 @@
+package uk.co.bbr.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.co.bbr.services.people.PersonService;
+import uk.co.bbr.services.people.dao.PersonDao;
+import uk.co.bbr.services.venues.VenueService;
+import uk.co.bbr.services.venues.dao.VenueDao;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/venues")
+@RequiredArgsConstructor
+public class VenueApiController {
+
+    private final VenueService venueService;
+
+    @GetMapping("/{slug}")
+    public Optional<VenueDao> getPerson(@PathVariable("slug") String slug) {
+        return this.venueService.fetchBySlug(slug);
+    }
+}
