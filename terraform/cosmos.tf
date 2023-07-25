141c1123c64cdf717a72b20cc2199f352d177e76
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-07-25 12:11:37 +0100

    Add cosmosdb to infrastructure

---
+++ terraform/cosmos.tf
@@ -0,0 +1,14 @@
+data "azurerm_cosmosdb_account" "bbr5" {
+  name                = terraform.workspace == "prod" ? "bbr5" : "bbr5-${terraform.workspace}"
+  resource_group_name = azurerm_resource_group.this.name
+
+  capabilities {
+    name = "EnableServerless"
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "example" {
+  name                = "locations"
+  resource_group_name = data.azurerm_cosmosdb_account.bbr5.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.bbr5.name
+}
---
+++ terraform/storage.tf
@@ -0,0 +1,12 @@
+resource "azurerm_storage_account" "bbr5" {
+  name                     = terraform.workspace == "prod" ? "bbr5-storage" : "bbr5-storage-${terraform.workspace}"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_queue" "location-updates" {
+  name                  = "location-updates"
+  storage_account_name  = azurerm_storage_account.bbr5.name
+}
