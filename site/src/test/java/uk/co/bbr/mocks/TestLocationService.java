f822435b5807f9e8450acaf98e19a83fbaa31744
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-07-26 14:10:58 +0100

    Update Venue location on save

--- site/src/test/java/uk/co/bbr/mocks/TestLocationService.java
+++ site/src/test/java/uk/co/bbr/mocks/TestLocationService.java
@@ -2,10 +2,12 @@ package uk.co.bbr.mocks;
 
 import uk.co.bbr.map.LocationService;
 import uk.co.bbr.services.bands.dao.BandDao;
+import uk.co.bbr.services.venues.dao.VenueDao;
 
 public class TestLocationService implements LocationService {
     @Override
-    public void updateBandLocation(BandDao band) {
-        return;
-    }
+    public void updateBandLocation(BandDao band) { }
+
+    @Override
+    public void updateVenueLocation(VenueDao venue) { }
 }
