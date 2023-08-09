d985072d318301c421efe076cb2a5fd5dc1ecb42
Tim Sawyer <tim.git@hotmandarins.co.uk>
2023-08-09 20:44:42 +0100

    Turn off csrf

--- site/src/main/java/uk/co/bbr/SecurityConfiguration.java
+++ site/src/main/java/uk/co/bbr/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package uk.co.bbr;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +12,19 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestHandler;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
 import uk.co.bbr.services.security.JwtService;
 import uk.co.bbr.web.security.filter.SecurityFilter;
+
+import java.io.IOException;
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,14 +37,15 @@ public class SecurityConfiguration {
 
         XorCsrfTokenRequestAttributeHandler delegate = new XorCsrfTokenRequestAttributeHandler();
         // set the name of the attribute the CsrfToken will be populated on
-        delegate.setCsrfRequestAttributeName("_csrf");
+        // set null to opt out of deferred tokens
+        delegate.setCsrfRequestAttributeName(null);
         // Use only the handle() method of XorCsrfTokenRequestAttributeHandler and the
         // default implementation of resolveCsrfTokenValue() from CsrfTokenRequestHandler
         CsrfTokenRequestHandler requestHandler = delegate::handle;
 
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and().authorizeHttpRequests((authz) -> authz
-              .requestMatchers("/**").permitAll()).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+        .and().authorizeHttpRequests(authz -> authz
+              .requestMatchers("/**").permitAll())
               .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint(SecurityFilter.URL_SIGN_IN))
         .and()
             .httpBasic()
@@ -45,8 +56,9 @@ public class SecurityConfiguration {
             .invalidateHttpSession(true)
             .deleteCookies(SecurityFilter.COOKIE_NAME)
         .and()
-            .csrf((csrf) -> csrf.csrfTokenRepository(this.csrfTokenRepository()).csrfTokenRequestHandler(requestHandler));
-
+            .csrf(csrf -> csrf.csrfTokenRepository(this.csrfTokenRepository()).csrfTokenRequestHandler(requestHandler).disable())
+            .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
 
 //        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -65,8 +77,24 @@ public class SecurityConfiguration {
 //                .and().addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
+    private static final class CsrfCookieFilter extends OncePerRequestFilter {
+
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+            CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+            // Render the token value to a cookie by causing the deferred token to be loaded
+            if (csrfToken != null) {
+                System.out.println("!!!" + csrfToken.getToken());
+            }
+
+            filterChain.doFilter(request, response);
+        }
+
+    }
+
     @Bean
     public CsrfTokenRepository csrfTokenRepository() {
-        return CookieCsrfTokenRepository.withHttpOnlyFalse();
+        return new HttpSessionCsrfTokenRepository();
     }
 }
