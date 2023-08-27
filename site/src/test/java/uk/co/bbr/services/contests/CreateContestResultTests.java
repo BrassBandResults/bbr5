c2c426e6243b40d3bfbc8f0732f3f73bf8af13a2
Tim <tim.git@hotmandarins.co.uk>
2023-08-27 21:53:14 +0100

    Event supports results with same band but different name (#185)

--- site/src/test/java/uk/co/bbr/services/contests/CreateContestResultTests.java
+++ site/src/test/java/uk/co/bbr/services/contests/CreateContestResultTests.java
@@ -24,6 +24,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @ActiveProfiles("test")
 @SpringBootTest(properties = {  "spring.config.location=classpath:test-application.yml",
@@ -77,7 +78,7 @@ class CreateContestResultTests implements LoginMixin {
         // arrange
         loginTestUser(this.securityService, this.jwtService, TestUser.TEST_MEMBER);
 
-        ContestDao contest = this.contestService.create("  London   Area      ");
+        ContestDao contest = this.contestService.create("  Welsh   Area      ");
         ContestEventDao event = this.contestEventService.create(contest, LocalDate.of(2020, 3, 3));
         BandDao band = this.bandService.create("  Black   Dyke   Band");
         PersonDao conductor1 = this.personService.create("Roberts", "John");
@@ -92,7 +93,7 @@ class CreateContestResultTests implements LoginMixin {
         // act
         ContestResultDao replaceResult = new ContestResultDao();
         replaceResult.setBand(band);
-
+        replaceResult.setBandName(band.getName());
         replaceResult.setDraw(2);
         replaceResult.setDrawSecond(3);
         replaceResult.setDrawThird(4);
@@ -171,15 +172,16 @@ class CreateContestResultTests implements LoginMixin {
         // act
         ContestResultDao replaceResult = new ContestResultDao();
         replaceResult.setBand(band);
+        replaceResult.setBandName("Rothwell Temperance");
         replaceResult.setDraw(3);
         replaceResult.setDrawSecond(4);
         replaceResult.setDrawThird(5);
         replaceResult.setNotes("Altered Note");
         replaceResult.setPointsTotal("999");
-        replaceResult.setPointsFirst("1st Updated");
-        replaceResult.setPointsSecond("2nd Updated");
-        replaceResult.setPointsThird("3rd Updated");
-        replaceResult.setPointsFourth("4th  Updated");
+        replaceResult.setPointsFirst("1st");
+        replaceResult.setPointsSecond("2nd");
+        replaceResult.setPointsThird("3rd");
+        replaceResult.setPointsFourth("4th ");
         replaceResult.setPointsPenalty("Updated");
         replaceResult.setPosition("5");
         replaceResult.setConductor(conductor3);
@@ -211,6 +213,105 @@ class CreateContestResultTests implements LoginMixin {
         assertEquals("4th Points", results.get(0).getPointsFourth());
         assertEquals("Penalty", results.get(0).getPointsPenalty());
         assertEquals("New Note", results.get(0).getNotes());
+
+        logoutTestUser();
+    }
+
+    @Test
+    void testAddingSameBandContestResultWithDifferentBandNameSucceeds() throws AuthenticationFailedException {
+        // arrange
+        loginTestUser(this.securityService, this.jwtService, TestUser.TEST_MEMBER);
+
+        ContestDao contest = this.contestService.create("  London   Area      ");
+        ContestEventDao event = this.contestEventService.create(contest, LocalDate.of(2020, 3, 3));
+        BandDao band = this.bandService.create("  Black Dyke ");
+        PersonDao conductor1 = this.personService.create("Simpson", "Peter");
+        PersonDao conductor2 = this.personService.create("Poet", "David");
+        PersonDao conductor3 = this.personService.create("Beckley", "Richard");
+
+        ContestResultDao existingResult = new ContestResultDao();
+        existingResult.setBand(band);
+        existingResult.setBandName("  Rothwell   Temperance  ");
+        existingResult.setDraw(2);
+        existingResult.setDrawSecond(3);
+        existingResult.setDrawThird(4);
+        existingResult.setNotes(" New Note ");
+        existingResult.setPointsTotal(" 234 ");
+        existingResult.setPointsFirst(" 1st Points ");
+        existingResult.setPointsSecond(" 2nd Points ");
+        existingResult.setPointsThird(" 3rd Points ");
+        existingResult.setPointsFourth(" 4th Points ");
+        existingResult.setPointsPenalty(" Penalty   ");
+        existingResult.setPosition("1");
+        existingResult.setConductor(conductor1);
+        existingResult.setConductorSecond(conductor2);
+        existingResult.setConductorThird(conductor3);
+        this.resultService.addResult(event, existingResult);
+
+        // act
+        ContestResultDao replaceResult = new ContestResultDao();
+        replaceResult.setBand(band);
+        replaceResult.setBandName("Wallace Arnold (Rothwell)");
+        replaceResult.setDraw(13);
+        replaceResult.setDrawSecond(14);
+        replaceResult.setDrawThird(15);
+        replaceResult.setNotes("Altered Note 2");
+        replaceResult.setPointsTotal("9992");
+        replaceResult.setPointsFirst("1st 2");
+        replaceResult.setPointsSecond("2nd 2");
+        replaceResult.setPointsThird("3rd 2");
+        replaceResult.setPointsFourth("4th  2");
+        replaceResult.setPointsPenalty("Updated 2");
+        replaceResult.setPosition("10");
+        replaceResult.setConductor(conductor1);
+        replaceResult.setConductorSecond(conductor2);
+        replaceResult.setConductorThird(conductor3);
+        ContestResultDao result = this.resultService.addResult(event, replaceResult);
+
+        // assert
+        ContestEventDao fetchedEvent = this.contestEventService.fetchEvent(event.getContest().getSlug(), event.getEventDate()).get();
+        List<ContestResultDao> results = this.resultService.fetchForEvent(fetchedEvent);
+        assertEquals(2, results.size());
+
+        assertEquals(existingResult.getId(), results.get(0).getId());
+        assertEquals("Black Dyke", results.get(0).getBand().getName());
+        assertEquals("Rothwell Temperance", results.get(0).getBandName());
+        assertEquals("Peter Simpson", results.get(0).getConductor().getName());
+        assertEquals("Peter Simpson", results.get(0).getOriginalConductorName());
+        assertEquals("David Poet", results.get(0).getConductorSecond().getName());
+        assertEquals("Richard Beckley", results.get(0).getConductorThird().getName());
+        assertEquals(1, results.get(0).getPosition());
+        assertEquals(ResultPositionType.RESULT, results.get(0).getResultPositionType());
+        assertEquals(2, results.get(0).getDraw());
+        assertEquals(3, results.get(0).getDrawSecond());
+        assertEquals(4, results.get(0).getDrawThird());
+        assertEquals("234", results.get(0).getPointsTotal());
+        assertEquals("1st Points", results.get(0).getPointsFirst());
+        assertEquals("2nd Points", results.get(0).getPointsSecond());
+        assertEquals("3rd Points", results.get(0).getPointsThird());
+        assertEquals("4th Points", results.get(0).getPointsFourth());
+        assertEquals("Penalty", results.get(0).getPointsPenalty());
+        assertEquals("New Note", results.get(0).getNotes());
+
+        assertNotEquals(existingResult.getId(), results.get(1).getId());
+        assertEquals("Black Dyke", results.get(1).getBand().getName());
+        assertEquals("Wallace Arnold (Rothwell)", results.get(1).getBandName());
+        assertEquals("Peter Simpson", results.get(1).getConductor().getName());
+        assertEquals("Peter Simpson", results.get(1).getOriginalConductorName());
+        assertEquals("David Poet", results.get(1).getConductorSecond().getName());
+        assertEquals("Richard Beckley", results.get(1).getConductorThird().getName());
+        assertEquals(10, results.get(1).getPosition());
+        assertEquals(ResultPositionType.RESULT, results.get(1).getResultPositionType());
+        assertEquals(13, results.get(1).getDraw());
+        assertEquals(14, results.get(1).getDrawSecond());
+        assertEquals(15, results.get(1).getDrawThird());
+        assertEquals("9992", results.get(1).getPointsTotal());
+        assertEquals("1st 2", results.get(1).getPointsFirst());
+        assertEquals("2nd 2", results.get(1).getPointsSecond());
+        assertEquals("3rd 2", results.get(1).getPointsThird());
+        assertEquals("4th  2", results.get(1).getPointsFourth());
+        assertEquals("Updated 2", results.get(1).getPointsPenalty());
+        assertEquals("Altered Note 2", results.get(1).getNotes());
 
         logoutTestUser();
     }
