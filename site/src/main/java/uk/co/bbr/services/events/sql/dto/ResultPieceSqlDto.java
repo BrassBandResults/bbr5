cacfb302f9d1faa7110ad226598a005236d023e3
Tim <tim.git@hotmandarins.co.uk>
2023-10-07 11:41:50 +0100

    Fix top query (#204)

--- site/src/main/java/uk/co/bbr/services/events/sql/dto/ResultPieceSqlDto.java
+++ site/src/main/java/uk/co/bbr/services/events/sql/dto/ResultPieceSqlDto.java
@@ -15,13 +15,6 @@ public class ResultPieceSqlDto extends AbstractSqlDto {
     private final String pieceYear;
     private final Long resultId;
     private final Integer pieceOrdering;
-    private final String composerSurname;
-    private final String composerFirstNames;
-    private final String composerSlug;
-    private final String arrangerSurname;
-    private final String arrangerFirstNames;
-    private final String arrangerSlug;
-
 
     public ResultPieceSqlDto(Object[] columnList) {
         this.pieceName = this.getString(columnList, 0);
@@ -29,12 +22,6 @@ public class ResultPieceSqlDto extends AbstractSqlDto {
         this.pieceYear = this.getString(columnList, 2);
         this.resultId = this.getLong(columnList,3);
         this.pieceOrdering = this.getInteger(columnList,4);
-        this.composerSurname = this.getString(columnList, 5);
-        this.composerFirstNames = this.getString(columnList, 6);
-        this.composerSlug = this.getString(columnList, 7);
-        this.arrangerSurname = this.getString(columnList, 8);
-        this.arrangerFirstNames = this.getString(columnList, 9);
-        this.arrangerSlug = this.getString(columnList, 10);
     }
 
     public ContestResultPieceDao asResultPiece() {
@@ -46,20 +33,6 @@ public class ResultPieceSqlDto extends AbstractSqlDto {
         returnPiece.setOrdering(this.pieceOrdering);
         returnPiece.setContestResult(new ContestResultDao());
         returnPiece.getContestResult().setId(this.resultId);
-
-        if (this.composerSlug != null) {
-            returnPiece.getPiece().setComposer(new PersonDao());
-            returnPiece.getPiece().getComposer().setSlug(this.composerSlug);
-            returnPiece.getPiece().getComposer().setFirstNames(this.composerFirstNames);
-            returnPiece.getPiece().getComposer().setSurname(this.composerSurname);
-        }
-
-        if (this.arrangerSlug != null) {
-            returnPiece.getPiece().setArranger(new PersonDao());
-            returnPiece.getPiece().getArranger().setSlug(this.arrangerSlug);
-            returnPiece.getPiece().getArranger().setFirstNames(this.arrangerFirstNames);
-            returnPiece.getPiece().getArranger().setSurname(this.arrangerSurname);
-        }
 
         return returnPiece;
     }
