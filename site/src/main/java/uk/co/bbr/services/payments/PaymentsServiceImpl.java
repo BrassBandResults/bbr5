3f032dfa367dd35c380d39f231c0be9739fc9a00
Tim <tim.git@hotmandarins.co.uk>
2024-03-10 14:18:45 +0000

    More subs (#277)

--- site/src/main/java/uk/co/bbr/services/payments/PaymentsServiceImpl.java
+++ site/src/main/java/uk/co/bbr/services/payments/PaymentsServiceImpl.java
@@ -74,7 +74,7 @@ public class PaymentsServiceImpl implements PaymentsService {
                 SubscriptionSearchParams
                     .builder()
                     .setQuery("status:'active'")
-                    .setLimit(100L)
+                    .setLimit(1000L)
                     .addExpand("data.customer")
                     .build();
 
@@ -97,7 +97,6 @@ public class PaymentsServiceImpl implements PaymentsService {
                 if (user.getStripeEmail() != null && user.getStripeEmail().equalsIgnoreCase(sub.getCustomerObject().getEmail())) {
                     if (sub.getCurrentPeriodEnd() != null) {
                         wrappedUser.setSubscriptionActive(true);
-                        wrappedUser.setDaysUntilDue(sub.getDaysUntilDue());
                         LocalDate endDateTime = Instant.ofEpochSecond(sub.getCurrentPeriodEnd()).atZone(ZoneId.systemDefault()).toLocalDate();
                         wrappedUser.setCurrentSubscriptionEndDate(endDateTime);
                         break;
