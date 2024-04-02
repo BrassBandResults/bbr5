f134b16ee015172995f00097c61b9a924b6a1cce
Tim Sawyer <tim.git@hotmandarins.co.uk>
2024-04-02 19:54:02 +0100

    Add new type for four section march contest

---
+++ site/src/main/resources/db/migration/V1_0009__oz_march_turnaround.sql
@@ -0,0 +1,4 @@
+INSERT INTO contest_type(old_id, updated, created, updated_by, created_by, name, slug, translation_key, draw_one_title, draw_two_title, draw_three_title, points_total_title, points_one_title, points_two_title, points_three_title, points_four_title, points_penalty_title, has_test_piece, has_own_choice, has_entertainments, statistics_show, statistics_limit) VALUES (null, '20240101 15:52:52', '20240101 11:37:27', 'owner', 'owner', 'March Contest (Inspection, Drill, Direction, Music)', 'march-inspection-drill-direction', 'contest-types.march-inspection-drill-direction','h.draw', null, null, 'h.total', 'h.inspection', 'h.drill', 'h.direction', 'h.music', null, 0, 1, 0, 0, '3');
+
+
+
