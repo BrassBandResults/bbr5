57f690c92611e899b25531899afe98d6f62cabed
Tim <tim.git@hotmandarins.co.uk>
2024-07-20 12:39:33 +0100

    Feature/add hk region (#298)
    * Old files tidy
    
    * Hong Kong region
    
    * Fix region test
    
    * Fix region test

---
+++ site/src/main/resources/db/migration/V1_0012__hk_region.sql
@@ -0,0 +1,1 @@
+INSERT INTO region(updated_by, created_by, name, slug, country_code, latitude, longitude, default_map_zoom) VALUES ('owner', 'owner', 'Hong Kong', 'hong-kong', 'hk', '22.39060', '114.15460', 6);
