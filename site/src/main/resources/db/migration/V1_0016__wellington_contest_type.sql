8bb0830984c6ec91dcb5d91d92225f1b3e0dfefd
Tim Sawyer <tim.git@hotmandarins.co.uk>
2025-09-16 18:34:44 +0100

    Wellington contest type

---
+++ site/src/main/resources/db/migration/V1_0016__wellington_contest_type.sql
@@ -0,0 +1,4 @@
+INSERT INTO contest_type(old_id, updated, created, updated_by, created_by, name, slug, translation_key, draw_one_title, draw_two_title, draw_three_title, points_total_title, points_one_title, points_two_title, points_three_title, points_four_title, points_penalty_title, has_test_piece, has_own_choice, has_entertainments, statistics_show, statistics_limit) VALUES (null, '20250916 19:24:02', '20250916 19:24:02', 'owner', 'owner', 'Sacred, Major Work (Own Choice), Solo and Entertainment', 'sacred-own-choice-solo-entertainment', 'contest-types.entertainments-two-programmes-contest','h.draw-one', null, null, 'h.total', 'h.sacred', 'h.own-choice', 'h.solo', 'h.entertainment', 'h.penalty', 0, 1, 1, 0, '0');
+
+
+
