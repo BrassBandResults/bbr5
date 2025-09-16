299985d2f91d75c30ad6260f322373e474de68a6
Tim Sawyer <tim.git@hotmandarins.co.uk>
2025-09-16 20:59:46 +0100

    Wellington corrections

---
+++ site/src/main/resources/db/migration/V1_0017__wellington_contest_type_corrections.sql
@@ -0,0 +1,4 @@
+UPDATE contest_type SET
+    translation_key = 'contest-types.sacred-own-choice-solo-entertainment',
+    draw_one_title = 'h.draw'
+WHERE slug = 'sacred-own-choice-solo-entertainment';
